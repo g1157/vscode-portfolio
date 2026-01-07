2d8c69781ec6e9496c18a56118660fd0266d8065
g1157 <g1157143860@gmail.com>
2026-01-08 03:00:22 +0800

    Update titles and meta tags to Blue_noblue

--- components/Head.tsx
+++ components/Head.tsx
@@ -10,19 +10,19 @@ const CustomHead = ({ title }: CustomHeadProps) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Blue_noblue is a developer and explorer building amazing web applications"
       />
       <meta
         name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
+        content="Blue_noblue, developer, explorer, web developer portfolio, vscode-portfolio"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
+      <meta property="og:title" content="Blue_noblue's Portfolio" />
       <meta
         property="og:description"
-        content="A full-stack developer building websites that you'd like to use."
+        content="A developer and explorer building amazing web applications."
       />
       <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:url" content="https://leleo.top" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
@@ -31,5 +31,5 @@ const CustomHead = ({ title }: CustomHeadProps) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Nitin Ranganath',
+  title: 'Blue_noblue',
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
-      <Head title={`Nitin Ranganath | ${pageProps.title}`} />
+      <Head title={`Blue_noblue | ${pageProps.title}`} />
       <Component {...pageProps} />
     </Layout>
   );
